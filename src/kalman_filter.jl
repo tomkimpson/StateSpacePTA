03dd9b604521eceeb280185061f0f8323561296d
Tom Kimpson <tomkimpson@gmail.com>
2023-03-04 20:00:35 +1100

    more exploration

--- src/kalman_filter.jl
+++ src/kalman_filter.jl
@@ -46,12 +46,12 @@ function kalman_filter(observations::Matrix{NF},
     #Calculate measurement noise matrix
     R = R_function(L,σm)
 
-    for i=1:3#size(observations)[2]
+    for i=1:3 #40 #size(observations)[2]
 
         observation = observations[:,i]
-        println(i)
-        println("Observations for this timestep are:")
-        display(observation)
+        println("Observation number: ", i)
+        #println("Observations for this timestep are:")
+        #display(observation)
         observation = reshape(observation,(1,size(observation)[1])) #can we avoid these reshapes and get a consistent dimensionality?
         ti = t[i]
 
@@ -75,6 +75,7 @@ function kalman_filter(observations::Matrix{NF},
 
         
         # #Weighted state predictions and covariance
+        println("---------GET X PREDICT")
         x_predicted, P_xx,Δx= predict(χ1, ukf_weights)
         P_xx += Q
 
@@ -85,12 +86,20 @@ function kalman_filter(observations::Matrix{NF},
         #Evolve the sigma vectors according to the measurement function
         #Note we are omitting Joe's extra step recalculating sigmas here 
         χ2 = H_function(χ,ti,dot_product,prefactor,ω,Φ0)
+        println("---------GET Y PREDICT")
         y_predicted, P_yy,Δy= predict(χ2, ukf_weights)
         P_yy += R
         
         #Measurement update 
         x,P = update(Δx, Δy,ukf_weights,P_xx,P_yy,observation,x_predicted,y_predicted)
 
+
+        println("The output x is = ",)
+        println(x)
+        println("The output of the 2nd row of P is =")
+        println(P[2,:])
+
+        println("============END OF ITERATION========================")
 
     end 
 
@@ -105,19 +114,20 @@ function calculate_sigma_vectors(x::Vector{NF},P::Matrix{NF},γ::NF,L::Int) wher
 
 
   
+    println("Calculate the sigma vectors")
 
-    
-    # println(ishermitian(P))
-    # display(P)
 
-    eps_matrix = diagm(fill(eps() ,47)) 
-    Pstable = NF(0.5)*(P + transpose(P)) + eps_matrix
+  
 
-    println("Attempting Cholesky of matrix with the following diagonal")
-    display(diag(P))
+    #eps_matrix = diagm(fill(eps() ,47)) 
+    #Pstable = NF(0.5)*(P + transpose(P)) + eps_matrix
+
+    #println("Attempting Cholesky of P matrix")
+    #display(diag(P))
     #display(P)
     #display(diag(P))
-    Psqrt = cholesky(P,check=true).L #the lower triangular part. This bit is returned by default in Python: https://numpy.org/doc/stable/reference/generated/numpy.linalg.cholesky.html
+    #https://discourse.julialang.org/t/is-this-a-bug-with-cholesky/16970/2
+    Psqrt = cholesky(Hermitian(P),check=true).L #the lower triangular part. This bit is returned by default in Python: https://numpy.org/doc/stable/reference/generated/numpy.linalg.cholesky.html
     M = γ .* Psqrt
 
   
@@ -179,8 +189,18 @@ function predict(χ::Matrix{NF}, W::UKF_weights) where {NF<:AbstractFloat}
     @unpack Wm,Wc = W
     
     x_predicted = Wm * χ #this is a dot product 
+
+    # println("You are inside the predict function")
+    # println("The weights used in the predict function are:")
+    # println(Wm)
+    # println("The sigma vector row used in the predict function is:")
+    # println(χ[2,:])
+
+    # println(χ[2,:])
+    # println(Wm)
+    # println(x_predicted)
+    # println("**************************")
     
-    println("You are inside the predict function")
     #println(x_predicted)
 
     y = χ .- x_predicted #from each row of χ, subtract the x predictions
@@ -190,18 +210,21 @@ function predict(χ::Matrix{NF}, W::UKF_weights) where {NF<:AbstractFloat}
     tmp = transpose(Wc) .* y #for each row in y, multiply it by the corresponding weight
     P_predicted = yT * tmp # size 47 x 47
 
-    println("the second row of the covar matrix is")
+    #println("the second row of the covar matrix is")
     
-    println("before_correction")
-    println(P_predicted[2,2], " ",P_predicted[2,3]," ",P_predicted[2,4])
+    #println("before_correction")
+    #println(P_predicted[2,2], " ",P_predicted[2,3]," ",P_predicted[2,4])
     eps_matrix = diagm(fill(eps(NF) ,47)) 
     
-    P_predicted = NF(0.5)*(P_predicted + transpose(P_predicted)) + eps_matrix# Enforce symmetry of the covariance matrix
-    println("after correction")
-    println(P_predicted[2,2]," ", P_predicted[2,3]," ",P_predicted[2,4])
+    P_predicted = NF(0.5)*(P_predicted + transpose(P_predicted)) + eps_matrix # Enforce symmetry of the covariance matrix
+    #println("after correction")
+    #println(P_predicted[2,2]," ", P_predicted[2,3]," ",P_predicted[2,4])
 
 
-    println("-------------------------")
+    #println("-------------------------")
+
+    println("The output from the predict function is")
+    println(x_predicted)
 
     return x_predicted, P_predicted,y
 
@@ -217,6 +240,7 @@ function update(Δx::Matrix{NF}, Δy::Matrix{NF},W::UKF_weights, P_xx::Matrix{NF
                observation::Matrix{NF},x_predicted::Matrix{NF},y_predicted::Matrix{NF}) where {NF<:AbstractFloat}
 
     @unpack Wc =W
+    println("You are inside the update function")
 
     #Get the cross correlation matrix
     yT = transpose(Δy)
@@ -241,8 +265,8 @@ function update(Δx::Matrix{NF}, Δy::Matrix{NF},W::UKF_weights, P_xx::Matrix{NF
 
 
     K = K3
-    println("writing")
-    writedlm("KalmanGain.txt", K)
+    #println("writing")
+    #writedlm("KalmanGain.txt", K)
    
 
     #Update state and covariance estiamtes
@@ -250,14 +274,19 @@ function update(Δx::Matrix{NF}, Δy::Matrix{NF},W::UKF_weights, P_xx::Matrix{NF
     innovation = transpose(observation - y_predicted)
     
 
-
+    println("x predicted is:")
+    println(x_predicted)
+    println("innovation is")
+    println(innovation)
+    println("Second line of Kalman gain is")
+    println(K[2,:])
     x = x_predicted - transpose(K*innovation)
     P = P_xx - K*P_yy*transpose(K)
     blob_factor = K*P_yy*transpose(K)
-    writedlm("blob_factor.txt", blob_factor)
+    #writedlm("blob_factor.txt", blob_factor)
 
-    println("The first row of the new covar matrix is:")
-    println(P[1,:])
+    #println("The first row of the new covar matrix is:")
+    #println(P[1,:])
 
 
     return vec(x),P
