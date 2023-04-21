60de2054b4c7b0ec38d4a25483fd73c1680ae1ca
Tom Kimpson <tomkimpson@gmail.com>
2023-04-21 18:26:09 +1000

    misc updates

--- py_src/slurm.sh
+++ py_src/slurm.sh
@@ -3,9 +3,9 @@
 #SBATCH --ntasks=1 
 #SBATCH --mem=8000MB 
 #SBATCH --time=48:00:00 
-#SBATCH --job-name=exponential_form_dynesty 
-#SBATCH --output=outputs/exponential_form_dynesty_out.txt 
+#SBATCH --job-name=exponential_form_dynestyV2 
+#SBATCH --output=outputs/exponential_form_dynestyV2_out.txt 
  
 source ~/.bashrc 
 conda activate OzStar 
-python main.py exponential_form_dynesty
+python main.py exponential_form_dynestyV2
