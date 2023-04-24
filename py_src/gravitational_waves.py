b4c53a37ffd17ac06a8be36f8d345f3e6f3ed5fe
Tom Kimpson <tomkimpson@gmail.com>
2023-04-24 13:23:08 +1000

    some clean up

--- py_src/gravitational_waves.py
+++ py_src/gravitational_waves.py
@@ -2,19 +2,19 @@ from numpy import sin,cos
 import numpy as np 
 from numba import jit 
 
+
+
+
+
+
+
+
 """
 Given the GW parameters, the pulsar parameters and the time,
 compute the frequency correction factor.
 Returns an object of shape (n times, n pulsars)
+Uses the exponential form of the equations and then selects the real part
 """
-
-
-
-
-
-
-
-#### EXP FORM
 @jit(nopython=True)
 def gw_prefactor_optimised_exp(delta,alpha,psi,q,q_products,h,iota,omega,d,t,phi0):
         
@@ -49,45 +49,12 @@ def gw_prefactor_optimised_exp(delta,alpha,psi,q,q_products,h,iota,omega,d,t,phi
         return np.real(GW_factor) #This has shape(n times, n pulsars)
 
 
-#### OLD FORM
-@jit(nopython=True)
-def gw_prefactor_optimised_old(delta,alpha,psi,q,q_products,h,iota,omega,d,t,phi0):
-
-        print("alpha = ", alpha)
-
-        m,n                 = principal_axes(np.pi/2.0 - delta,alpha,psi)    
-        gw_direction        = np.cross(m,n)
-      
-        dot_product         = 1.0 + np.dot(q,gw_direction) #matmul might be a bit faster, but np.dot has JIT support
-
-
-
-
-        e_plus              = np.array([[m[i]*m[j]-n[i]*n[j] for i in range(3)] for j in range(3)]) #tensordot might be a bit faster, but list comprehension has JIT support
-        e_cross             = np.array([[m[i]*n[j]-n[i]*m[j] for i in range(3)] for j in range(3)])
-
-        hp,hx               = h_amplitudes(h,iota) 
-        Hij                 = hp * e_plus + hx * e_cross
-        Hij_flat            = Hij.flatten()
-
-        hbar                = np.dot(Hij_flat,q_products)
-
-
-
-
-
-        prefactor    = 0.5*(hbar / dot_product)*(1.0 - cos(omega*d*dot_product))
-
-
-        tensor = np.outer(t,dot_product) #This has shape(n times, n pulsars)
-        time_variation = cos(-omega*tensor + phi0)
-
-        GW_factor = 1.0 - prefactor * time_variation
-        return GW_factor #This has shape(n times, n pulsars)
-
-
-
-#### TRIG FORM
+"""
+Given the GW parameters, the pulsar parameters and the time,
+compute the frequency correction factor.
+Returns an object of shape (n times, n pulsars)
+Uses the trigonometric form of the equations
+"""
 @jit(nopython=True)
 def gw_prefactor_optimised_trig(delta,alpha,psi,q,q_products,h,iota,omega,d,t,phi0):
 
