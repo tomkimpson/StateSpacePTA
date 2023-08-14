4388b3f4060ce5a7286ad961be10d03926edea9d
Tom Kimpson <tomkimpson@gmail.com>
2023-08-14 17:21:01 +1000

    added likelihood curves

--- notebooks/plot_likelihood_surfaces.py
+++ notebooks/plot_likelihood_surfaces.py
@@ -9,7 +9,18 @@ import matplotlib.pyplot as plt
 
 import matplotlib.pyplot as plt 
 fig = plt.figure(figsize=(6,6))
-ax = fig.add_subplot(111, projection='3d')
+#ax = fig.add_subplot(111, projection='3d')
+ax = fig.add_subplot(111)
+
+
+
+
+
+
+
+
+
+
 
 path1 = 'tmp_likelihood_surface.npz'
 path2 = 'tmp_likelihood_surface_earth.npz'
@@ -20,7 +31,7 @@ def load_and_plot(path,ax):
 
 
     iota_values = data['x']
-    h_values = np.log10(data['y'])
+    h_values = data['y']
     surface_pulsar = data['z']
     iota_idx, h_idx = np.unravel_index(surface_pulsar.argmax(), surface_pulsar.shape)
 
@@ -40,18 +51,24 @@ def load_and_plot(path,ax):
     z = np.reshape(surface_pulsar, (lx, ly))
    
     # Plot a 3D surface
-    ax.plot_surface(X, Y, z,alpha=0.5)
+    #ax.plot_surface(X, Y, z,alpha=0.5)
+
+    CS = ax.pcolormesh(X, Y, z) #clim=(0.0, 5e5)
+    #plt.contour(X, Y, Z, 4, colors='k')
+    plt.colorbar(CS)
+    ax.set_xscale('log')
+
 
 
     print(path, yc,xc,zc)
-    ax.scatter(yc,xc,zc, s=20)
+    #ax.scatter(yc,xc,zc, s=20)
  
 
 
 
-
+#load_and_plot('tmp_likelihood_surface2_earth.npz',ax)
 load_and_plot(path1,ax)
-load_and_plot(path2,ax)
+#load_and_plot(path2,ax)
 
 #ax.scatter(1e-12,1.0,-586119.6803021401,c='r', s=20)
 plt.show()
