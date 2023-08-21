74eda8704f17cfc7d31ace6c6e8cb29bd50e422d
Tom Kimpson <tomkimpson@gmail.com>
2023-08-21 15:12:04 +1000

    bayes, 2000 points

--- py_src/bilby_wrapper.py
+++ py_src/bilby_wrapper.py
@@ -33,7 +33,7 @@ def BilbySampler(KalmanFilter,init_parameters,priors,label,outdir):
                               sampler ='dynesty',
 			                  sample='rwalk_dynesty',
                               check_point_plot=False,
-                              npoints=1000,
+                              npoints=2000,
                               dlogz=1e-6,
                               #dlogz=0.1,
                               npool=1,
