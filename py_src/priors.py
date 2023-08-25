93e3596fc5e45875ad210398491a24dccf7647f6
Tom Kimpson <tomkimpson@gmail.com>
2023-08-25 13:49:14 +1000

    ...typo fix

--- py_src/priors.py
+++ py_src/priors.py
@@ -203,7 +203,7 @@ def set_prior_on_measurement_parameters(init_parameters,priors,measurement_model
         init_parameters["h"] = None
         #priors["h"] = bilby.core.prior.LogUniform(h/100.0, h*10.0, 'h')
         #priors["h"] = bilby.core.prior.LogUniform(1e-15, 1e-12, 'h')
-        priors["h"] = h #experiment fixing $h$ to compare performance in $\iota$
+        priors["h"] = P.h #experiment fixing $h$ to compare performance in $\iota$
 
 
     return init_parameters,priors 
