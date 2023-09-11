8247f5b6b8f4d130374ec34fe8bfb43682109b99
Tom Kimpson <tomkimpson@gmail.com>
2023-09-11 14:35:13 +1000

    additional unit test, pkg errors

--- test/test_gw.py
+++ test/test_gw.py
@@ -2,7 +2,9 @@
 from py_src import gravitational_waves
 import random 
 import numpy as np 
+from numpy import sin,cos
 
+"""Check that the magnitude of the principal axes is always unity"""
 def test_magnitude_of_principal_axes():
 
     N = 5
@@ -15,3 +17,16 @@ def test_magnitude_of_principal_axes():
 
         np.testing.assert_almost_equal(np.linalg.norm(m), 1.0) #https://numpy.org/doc/stable/reference/generated/numpy.testing.assert_almost_equal.html
         np.testing.assert_almost_equal(np.linalg.norm(n), 1.0) 
+
+
+        direction_inferred = np.cross(m,n)
+        direction_explicit = np.array([sin(thetas[i])*cos(phis[i]),sin(thetas[i])*sin(phis[i]),cos(thetas[i])])
+
+        #
+        np.testing.assert_almost_equal(direction_explicit,direction_inferred)
+
+
+# """ k \cross l should be the same as the direction unit vector"""
+# def test_principal_axes_cross_product():
+
+#     N = 1
