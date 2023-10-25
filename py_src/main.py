fe843b0cd5f3cde8f5a3b50fcc16b1e3d834dea6
Tom Kimpson <tomkimpson@gmail.com>
2023-10-25 18:18:28 +1100

    Revert "accidental jax edits"
    This reverts commit 424d4da129fc1f5421a2a89a17177d79e1e963f7.

--- py_src/main.py
+++ py_src/main.py
@@ -1,55 +1,20 @@
+import sys
+from run import bilby_inference_run,pp_plot_run
 
-
-from system_parameters import SystemParameters
-from pulsars import Pulsars
-from synthetic_data import SyntheticData
-
-
-from jax_kalman_filter import setup_kalman_machinery,kalman_filter
-
-
-import jax.numpy as np
-
-#Functional programming using JAX. 
+#Currently these are passed as command line arguments
+#Convenient, but could be better to setup a json/config file to read from for reproducibility 
+arg_name          = sys.argv[1]           # reference name
+h                 =  float(sys.argv[2])   # strain
+measurement_model =  sys.argv[3] # whether to use the H0(null) or H1(earth/pulsar) model
+seed              = int(sys.argv[4])          # the seeding
+num_gw_sources    =  int(sys.argv[5])
 
 
 if __name__=="__main__":
-
-
-
-    P   = SystemParameters(σp=None) 
-    PTA = Pulsars(P)                                       # setup the PTA
-    data = SyntheticData(PTA,P)                            # generate some synthetic data
-    y = data.f_measured
-    print("got the synthetic data")
-    y_jax = np.array(y)
+    bilby_inference_run(arg_name,h,measurement_model,seed,num_gw_sources)
 
 
 
 
-#Run the kalman filter once for optimal parameters to check everything works as expected
-        # F, Q, R,H_fn,T_fn = setup_kalman_machinery(P,PTA)
-
-    F, Q, R,X_factor,f_EM = setup_kalman_machinery(P,PTA)
 
 
-
-    initial_x = y[0,:]
-    initial_P = np.ones(len(initial_x)) * PTA.σm*1e3 
-
-
-    print(type(y_jax))
-    print(type(F))
-    print(type(Q))
-    print(type(R))
-    print(type(X_factor))
-    print(type(f_EM))
-    print(type(initial_x))
-    print(type(initial_P))
-
-   
-    x_result,P_result,l_result = kalman_filter(y_jax, F, Q, R, X_factor,f_EM,initial_x, initial_P)
-
-
-
-# x_hat, P,log_likelihood
