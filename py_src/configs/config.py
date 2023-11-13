7a0e1d2569894967bc47ba18b4d1b97c2373c019
Tom Kimpson <tomkimpson@gmail.com>
2023-11-13 13:15:03 +1030

    increase memory limits for k=20

--- py_src/configs/config.py
+++ py_src/configs/config.py
@@ -14,7 +14,7 @@ with open('slurm.sh','w') as g:
 
     g.write("#!/bin/bash \n \n")  
     g.write("#SBATCH --ntasks=1 \n")  
-    g.write("#SBATCH --mem=8000MB \n")  
+    g.write("#SBATCH --mem=16000MB \n")  
     g.write("#SBATCH --time=96:00:00 \n")  
     g.write(f"#SBATCH --job-name={arg_name} \n")  
     g.write(f"#SBATCH --output=outputs/{arg_name}_out.txt \n \n")
