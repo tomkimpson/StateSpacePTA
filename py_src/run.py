968a9b5df8ee6d042836849e79ff8884d98dd28e
Tom Kimpson <tomkimpson@gmail.com>
2023-11-23 08:26:33 +1100

    last typo fix before PP plot run

--- py_src/run.py
+++ py_src/run.py
@@ -54,4 +54,52 @@ def bilby_inference_run(arg_name,h,measurement_model,seed):
 
 
 
+def pp_plot_run(arg_name,h,measurement_model,seed,omega,phi_0,psi,delta,alpha):
+
+    logger = logging.getLogger().setLevel(logging.INFO)
+    #Setup the system
+    P   = SystemParameters(h=h,σp=None,σm=1e-11,use_psr_terms_in_data=True,measurement_model=measurement_model,seed=seed,Ω=omega,Φ0=phi_0,ψ=psi,α=alpha,δ=delta) # define the system parameters as a dict. Todo: make this a class
+    PTA = Pulsars(P)                                       # setup the PTA
+    data = SyntheticData(PTA,P)                            # generate some synthetic data
+
+    #Define the model 
+    model = LinearModel(P)
+
+    #Initialise the Kalman filter
+    KF = KalmanFilter(model,data.f_measured,PTA)
+
+    #Run the KF once with the correct parameters.
+    #This allows JIT precompile
+    optimal_parameters = priors_dict(PTA,P)
+    model_likelihood = KF.likelihood(optimal_parameters)
+    logging.info(f"Ideal likelihood given optimal parameters = {model_likelihood}")
+    
+    #Bilby
+    init_parameters, priors = bilby_priors_dict(PTA,P)
+   
+
+    logging.info("Testing KF using parameters sampled from prior")
+    params = priors.sample(1)
+
+
+    print("The sampled params are as follows")
+    print(params)
+
+
+    model_likelihood = KF.likelihood(params)
+    logging.info(f"Non -ideal likelihood for randomly sampled parameters = {model_likelihood}")
+
+    
+    # #Now run the Bilby sampler
+    npoints=2000
+    BilbySampler(KF,init_parameters,priors,label=arg_name,outdir="../data/nested_sampling/",npoints=npoints)
+    logging.info("The run has completed OK")
+
+
+
+
+
+
+
+
 
