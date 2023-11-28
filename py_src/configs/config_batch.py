26cdadb47f75c557a832f81ff7c57bc0e727e48e
Tom Kimpson <tomkimpson@gmail.com>
2023-11-29 08:18:48 +1100

    another Bayes run

--- py_src/configs/config_batch.py
+++ py_src/configs/config_batch.py
@@ -14,7 +14,7 @@ def create_slurm_job(arg_name,h,measurement_model,seed):
         g.write("#!/bin/bash \n \n")  
         g.write("#SBATCH --ntasks=1 \n")  
         g.write("#SBATCH --mem=8000MB \n")  
-        g.write("#SBATCH --time=24:00:00 \n")  
+        g.write("#SBATCH --time=48:00:00 \n")  
         g.write(f"#SBATCH --job-name={arg_name} \n")  
         g.write(f"#SBATCH --output=outputs/{arg_name}_out.txt \n \n")
 
@@ -28,21 +28,38 @@ def create_slurm_job(arg_name,h,measurement_model,seed):
 
 
 
-#Bayes vs h0, multiple noise
-N = 10
-seeds = np.arange(1235+10,1235+10+N,1)
-#trains = [5e-15,1e-14,5e-14,1e-13,5e-13,1e-12]
-strains = [5e-13,1e-12]
-models = ["pulsar"]
-#models = ["pulsar","earth"]
-#models = ["null"]
+
+#Bayes vs h0, single noise
+
+strains = np.logspace(-15,-13,40)
+models = ["pulsar", "earth", "null"]
+seeds = [1250,1251,1253] #try a few different seeds for comparison
 with open('batch.sh','w') as b: 
     for s in seeds:
         for h in strains:
             for m in models:
-                arg_name = f"eg_canonical_bayes_{m}_{h}_{s}"
+                arg_name = f"bayes_dlogz_{m}_{h}_{s}"
                 create_slurm_job(arg_name,h,m,s)
                 b.write(f"sbatch slurm_jobs/slurm_{arg_name}.sh & \n")
+
+
+
+
+# #Bayes vs h0, multiple noise
+# N = 10
+# seeds = np.arange(1235+10,1235+10+N,1)
+# #trains = [5e-15,1e-14,5e-14,1e-13,5e-13,1e-12]
+# strains = [5e-13,1e-12]
+# models = ["pulsar"]
+# #models = ["pulsar","earth"]
+# #models = ["null"]
+# with open('batch.sh','w') as b: 
+#     for s in seeds:
+#         for h in strains:
+#             for m in models:
+#                 arg_name = f"eg_canonical_bayes_{m}_{h}_{s}"
+#                 create_slurm_job(arg_name,h,m,s)
+#                 b.write(f"sbatch slurm_jobs/slurm_{arg_name}.sh & \n")
 
 
 
